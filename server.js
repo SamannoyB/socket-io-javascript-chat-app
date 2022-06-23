ebdd822d5166b3fe566c82ad0ca14a816e4816ec
SamannoyB <82638503+SamannoyB@users.noreply.github.com>
2022-06-23 17:22:47 +0530

    Update server.js

--- server.js
+++ server.js
@@ -1,16 +1,7 @@
 // here all our server code shall go 
 
-const http = require('http');
-const express = require('express');
-const app = express();
-const server = http.createServer(app);
-const path = require('path');
+const io = require('socket.io')('http://spotchat-chat-app.herokuapp.com');
 
-app.use(express.static(__dirname + '/public'));
-// the problem was we shouldn't have given ./
-
-const io = require('socket.io')(server);
-const PORT = 5000;
 
 io.on('connection', (socket) => {
     socket.on('join-user', (userMsg) => {
@@ -23,7 +14,3 @@ io.on('connection', (socket) => {
         io.emit('chat message', user, msg);
     })
 })
-
-server.listen(PORT, () => {
-    console.log(`Server is Listening On Port ${PORT}`);
-});
